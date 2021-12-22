0e8d43528108f4ca23385212cdb127a717797370
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-12-22 22:59:04 +0530

    Add files via upload

---
+++ Day17/script.js
@@ -0,0 +1,83 @@
+// const Section1 = document.querySelector(".first_section");
+// console.log(document);
+// Section1.style.background = "green";
+
+const myhtml = `
+    <h1>Myself Abhishek A MERN Stack Developer</h1>
+    `;
+
+const PankajCutsomText = `
+    <p>   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse unde ea nesciunt, obcaecati, et voluptates amet
+    quibusdam odit repellat ab error, recusandae debitis eum nam eveniet aliquam maiores labore itaque.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse unde ea nesciunt, obcaecati, et voluptates amet
+    quibusdam odit repellat ab error, recusandae debitis eum nam eveniet aliquam maiores labore itaque.</p>
+    
+    `;
+
+// Section1.insertAdjacentHTML("beforeend", "ABHISHEK");
+// Section1.insertAdjacentHTML("beforebegin", "PANKAJ");
+// Section1.insertAdjacentHTML("afterbegin", myhtml);
+// Section1.insertAdjacentHTML("afterend", PankajCutsomText);
+
+// console.log(
+//   "pankaj pankaj pankaj pakaj pankaj pankaj pankaj pakaj pankaj pankaj pankaj pakaj pankaj pankaj pankaj  pankaj pankaj pankaj pakaj pankaj pankaj pankaj  pankaj pankaj pankaj pakaj pankaj pankaj pankaj  pankaj pankaj pankaj pakaj pankaj pankaj pankaj "
+// );
+
+// for (i = 100; i > 0; i--) {
+//   console.log(i);
+// }
+
+// for (i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// const Loan = 2000;
+
+// if (Loan !== 2000 && Loan === 2000) {
+//   console.log("BHUL HAI");
+// } else if (Loan <= 2000 || Loan === 20000) {
+//   console.log("AYA HAI LOAN");
+// } else {
+//   console.log("NAHI HAI LOAN");
+// }
+
+// let i = 0;
+// while (i < 100) {
+//   console.log(i);
+//   i++;
+// }
+
+let Goal = 20;
+// console.log(typeof Goal);
+
+let RameshSirname = "Desai";
+// console.log(typeof RameshSirname);
+
+let pass = true;
+// console.log(typeof pass);
+
+let PankajDetails = {
+  name: "Pankaj",
+  ID: "19012060",
+  phone: "100",
+  Address: "EARTH",
+};
+
+// console.log(PankajDetails);
+// console.log(PankajDetails.name);
+// console.log(PankajDetails.ID);
+// console.log(PankajDetails.phone);
+
+// FIND THE AREA OF THE CIRCLE WHOSE RADIUS IS 5CM ?
+// AREA OF TEH CIRCLE =3.14*5*5)
+
+// console.log(Math.PI);
+// console.log(Math.PI * 5 * 5);
+
+let a = 20;
+let b = 30;
+console.log(a + b);
+
+let c = 2340;
+let d = 12330;
+console.log(c + d);
