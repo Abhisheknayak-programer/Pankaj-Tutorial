27c59da2408d0a1eedcaaa3828b3b19fed8a6527
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-12-22 21:23:07 +0530

    Add files via upload

---
+++ Day16/script.js
@@ -0,0 +1,58 @@
+let x = 65;
+console.log(x);
+x = 41;
+console.log(x);
+
+const pankajSirname = "Advani";
+console.log(pankajSirname);
+// pankajSirname = "Desai";
+
+const pankajAge = 22;
+
+if (pankajAge >= 22) {
+  console.log("JALD SADI KARAO");
+} else {
+  console.log("GHAR SE BHAGO");
+}
+
+const RameshSirname = "VADVANI";
+
+if (RameshSirname === "VADVANI") {
+  console.log("GATE KHOLO");
+} else {
+  console.log("MAT KHOLO");
+}
+
+const TotalGoal = "2";
+
+if (TotalGoal > 2) {
+  console.log("KHEL KHATAM KARO");
+} else if (TotalGoal < 1) {
+  console.log("KHEL SURU KARO");
+} else if (TotalGoal === 2) {
+  console.log("JIT GAYE");
+} else {
+  console.log("DRAW");
+}
+
+const Chor = 40;
+const Police = 1;
+const People = 200;
+const Signal = "KHUL JA SIM SIM";
+
+if (Chor === 41 || Signal === "KHUL JA SIM SIM" || Police === 200) {
+  console.log("KHUL GAYA MAI");
+} else if (Chor === 40 && Police === 10) {
+  console.log("BHAG POLICE BHAG");
+} else {
+  console.log("People CHOR AA RAHE HAI CHUPA LOO SAMAN");
+}
+
+const RaviTeja = "BUDHA";
+const SalamKhan = "BACHHA";
+
+if (RaviTeja !== SalamKhan) {
+  console.log("MISMATCHED");
+} else {
+  console.log("MATCH THAI GAYO");
+}
